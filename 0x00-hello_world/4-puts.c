81c1ef724d66df9a180f788892a64c90fe8d0298
Lona <matshinganalona@gmail.com>
2023-02-16 05:40:07 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h
-/** 
- * main - A program that prints a line with puts function 
- * return 0
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
