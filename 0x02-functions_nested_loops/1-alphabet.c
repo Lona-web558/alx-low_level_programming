26934992915ad37fc0a3411ed979704ad98b57ce
Lona <matshinganalona@gmail.com>
2023-02-21 05:13:16 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * REturn: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar('a');
+	}
+	putchar('\n');
+	return (0);
+}
