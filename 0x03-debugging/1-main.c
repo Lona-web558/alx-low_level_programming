524fb27014db6e404c65cb6985ea275d9ec9c350
Lona <matshinganalona@gmail.com>
2023-02-24 13:09:05 -0800

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print program
+ * Return 0
+ */
+int main(void)
+{
+	int i;
+	printf("Infinit loop:(\n");
+	i = 0;
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
+
