4cd41195c0e6c79f12af6e0981ca016f15f0f515
Lona <matshinganalona@gmail.com>
2023-02-26 22:09:18 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of int a
+ *
+ * fitdt int
+ * second knt
+ * return 0
+ */
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
+
